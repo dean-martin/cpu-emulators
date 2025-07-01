9a54c88458b4f17a498bc011afbe8fc74e92a975
Dean Martin <contact@deanmartin.pro>
2025-07-01 12:32:37 -0400

    weird tabness

--- src/8080.cc
+++ src/8080.cc
@@ -86,15 +86,15 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
     {
 	case 0x00: printf("NOP"); break;
 	case 0x01: printf("LXI\tB,#$%02x%02x", code[2], code[1]); opbytes = 3; break;
-        case 0x02: printf("STAX   B"); break;    
-        case 0x03: printf("INX    B"); break;    
-        case 0x04: printf("INR    B"); break;    
-        case 0x05: printf("DCR    B"); break;    
-        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
-        case 0x07: printf("RLC"); break;    
-        case 0x08: printf("NOP"); break;    
-        case 0x0f: printf("RRC"); break;    
-        case 0x21: printf("LXI H, #$%02x%02x", code[2], code[1]); opbytes = 3; break;    
+	case 0x02: printf("STAX   B"); break;    
+	case 0x03: printf("INX    B"); break;    
+	case 0x04: printf("INR    B"); break;    
+	case 0x05: printf("DCR    B"); break;    
+	case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
+	case 0x07: printf("RLC"); break;    
+	case 0x08: printf("NOP"); break;    
+	case 0x0f: printf("RRC"); break;    
+	case 0x21: printf("LXI H, #$%02x%02x", code[2], code[1]); opbytes = 3; break;    
 	case 0x32: printf("STA ($%02x%02x)", code[2], code[1]); opbytes = 3; break;
 	case 0x35: printf("DCR M"); break;
 	case 0x3a: printf("LDA ($%02x02x)", code[2], code[1]); opbytes = 3; break;
