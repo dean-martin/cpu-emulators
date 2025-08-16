4a09218eb3bc2e6c53c6b4235a8aa62b9f0e4bba
Dean Martin <contact@deanmartin.pro>
2025-08-16 05:27:04 -0400

    fmtin

--- src/sdl_8080.cc
+++ src/sdl_8080.cc
@@ -16,7 +16,7 @@ const int WindowHeight = 360;
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Hello World", WindowWidth, WindowHeight, NULL, &window, &renderer)) {
+	if (!SDL_CreateWindowAndRenderer("Hello World", WindowWidth, WindowHeight, NULL, &window, &renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -39,8 +39,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_KEY_DOWN ||
-        event->type == SDL_EVENT_QUIT) {
+    if(event->type == SDL_EVENT_KEY_DOWN)
+    {
+		// @TODO: check for Esc or Q.
+		return SDL_APP_SUCCESS;
+    }
+    if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
     return SDL_APP_CONTINUE;
@@ -62,35 +66,29 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
         // @TODO: don't use u8 for a 1 bit value.
         u8 *Pixel = (u8 *)VideoPixels;
-        for(int Y=0;
-                Y < 224;
-                ++Y)
+        for(int Y=0; Y < 224; ++Y)
         {
-            for(int X=0;
-                    X < 256/8;
-                    ++X, ++VideoRAM)
+            for(int X=0; X < 256/8; (++X, ++VideoRAM))
             {
                 if((VideoRAM - GlobalCPU.memory) > 0x3FFF)
                 {
-                        printf("[ERROR] VideoRAM overflowed: 0x%x", (VideoRAM - GlobalCPU.memory));
-                        exit(1);
+					printf("[ERROR] VideoRAM overflowed: 0x%x", (VideoRAM - GlobalCPU.memory));
+					exit(1);
                 }
 
-                for(int I=0;
-                        I<8;
-                        ++I)
+                for(int I=0; I<8; ++I)
                 {
-                        if((*VideoRAM >> I) & 1)
-                                *Pixel++ = 1;
-                        else
-                                *Pixel++ = 0;
+					if((*VideoRAM >> I) & 1)
+						*Pixel++ = 1;
+					else
+						*Pixel++ = 0;
                 }
 
 #if 0
                 if (*VideoRAM != 0)
                 {
-                                printf("0x%x ", *VideoRAM);
-                                PrintBinary(*VideoRAM);
+					printf("0x%x ", *VideoRAM);
+					PrintBinary(*VideoRAM);
                 }
 #endif
 			}
@@ -102,18 +100,14 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         int X2 = 0;
         int Y2 = 0;
-        for(int X=256-1;
-                X>=0;
-                --X, ++Y2, X2=0)
+        for(int X=256-1; X>=0; (--X, ++Y2, X2=0))
         {
-            for(int Y=0;
-                    Y<224;
-                    ++Y, X2++)
+            for(int Y=0; Y<224; (++Y, ++X2))
             {
                 u8 *GamePixel = (u8 *)(VideoPixels + (X) + (Y*256));
                 if (*GamePixel)
                 {
-                    SDL_RenderPoint(renderer, X2, Y2);
+					SDL_RenderPoint(renderer, X2, Y2);
                 }
             }
         }
